7bbfe388e0a129f0ad24b9188b9d6730a0a13e8e
Hendrio Aguiar <hendrioninth3015@gmail.com>
2025-10-14 21:00:52 -0300

    novos perfis

--- util/perfis.py
+++ util/perfis.py
@@ -17,7 +17,8 @@ class Perfil(str, Enum):
 
     # PERFIS DO SEU SISTEMA #####################################
     ADMIN = "Administrador"
-    CLIENTE = "Cliente"
+    ESTUDANTE = "Estudante"
+    RECRUTADOR = "Recrutador"
     # FIM DOS PERFIS ############################################
 
     def __str__(self) -> str:
